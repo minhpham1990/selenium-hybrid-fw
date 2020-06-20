b29071fea230f1ffa2e636767fcd6d9d7d47cca3
minhpham <minh@iscity.com.my>
2020-06-20 18:39:19 +0800

    Add some test class

--- testcases/com/guru/user/User_01_Login.java
+++ testcases/com/guru/user/User_01_Login.java
@@ -3,12 +3,12 @@ package com.guru.user;
 import org.testng.annotations.Test;
 
 public class User_01_Login {
-  @Test
-  public void Register_01_EmptyAllField() {
+  @Test(description = "001 - Leave blank all field")
+  public void Login_01_EmptyAllField() {
   }
   
-  @Test
-  public void Register_02_InvalidEmail() {
+  @Test(description = "002 - Enter invalid email format")
+  public void Login_02_InvalidEmail() {
   }
   
 }
---
+++ testcases/com/guru/user/User_01_Order.java
@@ -0,0 +1,9 @@
+package com.guru.user;
+
+import org.testng.annotations.Test;
+
+public class User_01_Order {
+  @Test
+  public void Order_01_EmptyAllField() {
+  }
+}
